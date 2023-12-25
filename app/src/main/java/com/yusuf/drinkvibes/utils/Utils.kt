6f080fba84df18f587e105840a521736b7611acd
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-12-25 15:08:54 +0300

    Utils (Base Url and Get Urls)

---
+++ app/src/main/java/com/yusuf/drinkvibes/utils/Utils.kt
@@ -0,0 +1,19 @@
+package com.yusuf.drinkvibes.utils
+
+class Utils {
+
+    companion object{
+
+        const val BASE_URL= "https://raw.githubusercontent.com/"
+
+        const val GET_MOODS_URL= "/Yusuf-Solmaz/MyData/main/Beverages/Moods.json"
+        const val GET_COMFORTABLE_URL= "/Yusuf-Solmaz/MyData/main/Beverages/Comfortable.json"
+        const val GET_ENERGETIC_URL= "/Yusuf-Solmaz/MyData/main/Beverages/Energetic.json"
+        const val GET_EXCITED_URL= "/Yusuf-Solmaz/MyData/main/Beverages/Excited.json"
+        const val GET_HAPPY_URL= "/Yusuf-Solmaz/MyData/main/Beverages/Happy.json"
+        const val GET_ROMANTIC_URL= "/Yusuf-Solmaz/MyData/main/Beverages/Romantic.json"
+        const val GET_SAD_URL= "/Yusuf-Solmaz/MyData/main/Beverages/Sad.json"
+        const val GET_STRESSFUL_URL= "/Yusuf-Solmaz/MyData/main/Beverages/Stressful.json"
+        const val GET_TIRED_URL= "/Yusuf-Solmaz/MyData/main/Beverages/Tired.json"
+    }
+}
