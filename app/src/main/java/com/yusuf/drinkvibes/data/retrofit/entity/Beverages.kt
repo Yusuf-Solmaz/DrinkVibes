86ff0f2deee33a46deda8879b80aaf85a3d0f6e3
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-12-25 14:38:10 +0300

    Data Access Objects Updated

--- app/src/main/java/com/yusuf/drinkvibes/data/retrofit/entity/Beverages.kt
+++ app/src/main/java/com/yusuf/drinkvibes/data/retrofit/entity/Beverages.kt
@@ -1,11 +1,27 @@
 package com.yusuf.drinkvibes.data.retrofit.entity
 
+import com.google.gson.annotations.SerializedName
+
 data class Beverages(
-    val beverageName: String,
-    val contents: String,
+
+    @SerializedName("id")
     val id: Int,
+
+    @SerializedName("beverageName")
+    val beverageName: String,
+
+    @SerializedName("contents")
+    val contents: String,
+
+    @SerializedName("imageUrl")
     val imageUrl: String,
+
+    @SerializedName("mood")
     val mood: String,
+
+    @SerializedName("preparation")
     val preparation: String,
+
+    @SerializedName("youtubeVideoId")
     val youtubeVideoId: String
 )
--- app/src/main/java/com/yusuf/drinkvibes/data/retrofit/entity/Moods.kt
+++ app/src/main/java/com/yusuf/drinkvibes/data/retrofit/entity/Moods.kt
@@ -1,6 +1,12 @@
 package com.yusuf.drinkvibes.data.retrofit.entity
 
+import com.google.gson.annotations.SerializedName
+
 data class Moods(
+
+    @SerializedName("id")
     val id: Int,
+
+    @SerializedName("moodName")
     val moodName: String
 )
