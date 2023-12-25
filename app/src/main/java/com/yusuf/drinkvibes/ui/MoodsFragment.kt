53efea0e1ea75e3224892e300f82d61b818e969a
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-12-25 15:18:48 +0300

    UI

---
+++ app/src/main/java/com/yusuf/drinkvibes/ui/MoodsFragment.kt
@@ -0,0 +1,24 @@
+package com.yusuf.drinkvibes.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yusuf.drinkvibes.R
+
+class MoodsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_moods, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
