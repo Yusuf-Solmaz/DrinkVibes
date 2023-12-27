5efb3106da479a06f03612b4be37a5b04ff84239
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-12-27 18:21:30 +0300

    Update

--- app/src/main/java/com/yusuf/drinkvibes/data/roomdb/service/FavouriteBeveragesService.kt
+++
@@ -1,9 +0,0 @@
-package com.yusuf.drinkvibes.data.roomdb.service
-
-import com.yusuf.drinkvibes.data.roomdb.entity.FavouriteBeverages
-
-interface FavouriteBeveragesService {
-
-    suspend fun getAllFavouriteBeverages() : List<FavouriteBeverages>
-
-}
