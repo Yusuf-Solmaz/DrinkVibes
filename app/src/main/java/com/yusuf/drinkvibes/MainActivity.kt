ad8b750501ee5ffc4f8f10fba7a2044f01449558
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-01-01 16:06:45 +0300

    Status Bar Hided

--- app/src/main/java/com/yusuf/drinkvibes/MainActivity.kt
+++ app/src/main/java/com/yusuf/drinkvibes/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.yusuf.drinkvibes
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.replace
 import androidx.navigation.fragment.NavHostFragment
@@ -20,6 +23,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
 
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
---
+++ app/src/main/java/com/yusuf/drinkvibes/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.yusuf.drinkvibes
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
